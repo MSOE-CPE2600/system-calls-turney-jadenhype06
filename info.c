843a812d27091c9fee7f973fa30a843bf5ad03ec
jadenhype06 <hipej@msoe.edu>
2025-10-31 11:43:08 -0500

    info.c finished

--- info.c
+++ info.c
@@ -1,9 +1,42 @@
-// Name, etc
+/* 
+* Filename: info.c
+* Author: Jaden Hipe
+* Date: 10/30/2025
+* Compile info: make info.c
+* Description: Provides maintenance information using system calls.
+*/ 
 
-// info.c
-
+#include <time.h>
+#include <sys/utsname.h>
+#include <sys/sysinfo.h>
+#include <stdio.h>
+#include <unistd.h>
 int main(int argc, char* argv[])
 {
+    //Using clock_gettime
+    struct timespec spec;
+    int time = clock_gettime(CLOCK_REALTIME, &spec);
+    printf("Time (ns): %ld\n", spec.tv_nsec);
+    
+
+    //uname
+    struct utsname machine_info;
+    uname(&machine_info);
+    printf("Network Name: %s\n",machine_info.nodename);
+    printf("System Name: %s\n",machine_info.sysname);
+    printf("OS Release: %s\n",machine_info.release);
+    printf("OS Version: %s\n",machine_info.version);
+    printf("Hardware Type: %s\n",machine_info.machine);
+
+    // //Number of Processors
+    printf("Number of Processors: %d\n", get_nprocs());
+
+    //Number of Bytes
+    long phys_bytes = sysconf(_SC_PHYS_PAGES) * getpagesize();
+    long free_bytes = sysconf(_SC_AVPHYS_PAGES) * getpagesize();
+    printf("Amount of Physical Memory: %ld\n", phys_bytes);
+    printf("Amount of Free Memory: %ld\n", free_bytes);
 
     return 0;
 }
+
--- pinfo.c
+++ pinfo.c
@@ -1,9 +1,22 @@
-// Name, etc
+/* 
+* Filename: pinfo.c
+* Author: Jaden Hipe
+* Date: 10/31/2025
+* Compile info: make pinfo.c
+* Description: Provides pid information using system calls.
+*/ 
 
-// pinfo.c
+#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
+    pid_t mypid = getpid();
 
+    //Print out pid info
+    printf("Process Priority: %d\n", getpriority(mypid));
+    printf("Scheduling Method: %d\n", sched_getscheduler(mypid));
     return 0;
 }
